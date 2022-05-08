ea78375f805208a86ba8776964d0269c3b42798a
anywhy <yangdaixing168@163.com>
2022-05-08 23:09:52 +0800

    add more test case

--- assets_test.go
+++ assets_test.go
@@ -6,7 +6,25 @@ func TestClient_Assets(t *testing.T) {
 	var cli = newClient()
 	var list, err = cli.Assets(ctx, &AssetsRequest{
 		OrderDirection: "desc",
+		Offset:         0,
 		Limit:          10,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for _, c := range list {
+		t.Log(c)
+	}
+}
+
+func TestClient_Assets_IncludeOrders(t *testing.T) {
+	var cli = newClient()
+	var list, err = cli.Assets(ctx, &AssetsRequest{
+		OrderDirection: "desc",
+		Offset:         0,
+		Limit:          10,
+		IncludeOrders:  true,
 	})
 	if err != nil {
 		t.Error(err)
